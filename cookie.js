052726d1500d0ccdf29de8e3a9fe4c5cc017b179
Seokbong <dfbgq346q@gmail.com>
2022-04-13 10:12:04 +0900

    *

--- cookie.js
+++ cookie.js
@@ -1,1 +1,11 @@
 const http = require('http');
+
+http.createServer((req, res) => {
+    console.log('req url : ', req.url);
+    console.log('req.headers.cookie : ', req.headers.cookie);
+    res.writeHead(200, { 'Set-Cookie' : 'myCookie=seokbong' });
+    res.end('Hello Cookie');
+})
+    .listen(8080, () => {
+        console.log("8080 PORT ON");
+    });
